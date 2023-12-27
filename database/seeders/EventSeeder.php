a735a5616e787b255d4c207715f36ba2759d21e0
Lara Vieira <contato@laravieira.me>
2023-12-27 16:13:37 -0300

    Improve image rendering.

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
 
 class EventSeeder extends Seeder
 {
@@ -12,6 +14,27 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Event::factory()->create([
+            'name' => 'Laravel Event',
+            'slug' => 'laravel-event',
+            'description' => 'Laravel events are a way to trigger and handle specific actions or tasks within your application. They follow the observer pattern, where an event is fired or raised, and one or more listeners are responsible for handling that event.',
+            'image' => 'public/events/eIbWoeX1ISvaDcKuTrdZDgCatJhb6b4joKIu2cBK.jpg',
+            'country_id' => 1,
+            'city_id' => 1,
+            'user_id' => 1,
+            'start_date' => new Date('2021-10-10'),
+            'end_date' => new Date('2021-10-12'),
+        ]);
+        Event::factory()->create([
+            'name' => 'Vue.js Event',
+            'slug' => 'vuejs-event',
+            'description' => 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications.',
+            'image' => 'public/events/nZY71bXNAKCto5guMtbhjBru2Y5Uo0DHTqjSHI01.jpg',
+            'country_id' => 1,
+            'city_id' => 1,
+            'user_id' => 1,
+            'start_date' => new Date('2021-10-10'),
+            'end_date' => new Date('2021-10-12'),
+        ]);
     }
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -104,7 +104,7 @@
                                id="image"
                                accept="image/*"
                                  x-on:change="onFileChange($event)"
-                               class="block hidden w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"/>
+                               class="hidden w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"/>
                         <div class="flex justify-stretch">
                             <label x-show="!file" for="image" class="flex justify-between items-center gap-2 transition-all duration-1000 w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
                                 {{ __('Upload Image') }}
